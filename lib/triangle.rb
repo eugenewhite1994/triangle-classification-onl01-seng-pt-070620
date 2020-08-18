e556d29a4072900f7de893cfa5120d311040fb3e
Eugene White <eugenewhite1994@gmail.com>
2020-08-18 22:49:54 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,40 +1,31 @@
-require 'pry'
 class Triangle
-  def initialize(side_1, side_2, side_3)
-    @triangle_sides = []
-    @triangle_sides << side_1
-    @triangle_sides << side_2
-    @triangle_sides << side_3
-  end
 
-  def valid?
-    sum_one_two = @triangle_sides[0] + @triangle_sides[1]
-    sum_one_three = @triangle_sides[0] + @triangle_sides[2]
-    sum_two_three = @triangle_sides[1] + @triangle_sides[2]
+    attr_accessor
 
-    if (@triangle_sides.none? {|side| side <= 0}) &&
-      (sum_one_two > @triangle_sides[2] && sum_one_three > @triangle_sides[1] && sum_two_three > @triangle_sides[0])
-      return true
-    else
-      return false
-    end
+  def initialize(length1, length2, length3)
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
   end
 
   def kind
-    if valid?
-      if @triangle_sides.uniq.length == 1
-        return :equilateral
-      elsif @triangle_sides.uniq.length == 2
-        return :isosceles
-      else
-        return :scalene
+    if @length1 <= 0 || @length2 <= 0 || @length3 <= 0
+      begin
+        raise TriangleError
       end
-    else
-      raise TriangleError
+    elsif @length1 + @length2 <= @length3 || @length1 + @length3 <= @length2 || @length2 + @length3 <= @length1
+      begin
+        raise TriangleError
+      end
+    elsif @length1 == @length2 && @length2 == @length3
+        :equilateral
+    elsif @length1 == @length2 || @length2 == @length3 || @length1 == @length3
+        :isosceles
+    else @length1 != @length2 || @length2 != @length3 || @length1 != @length3
+        :scalene
     end
   end
-end
 
-class TriangleError < StandardError
-
-end
+  class TriangleError < StandardError
+  end
+end	end
